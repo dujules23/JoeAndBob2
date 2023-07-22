3e42c79506713bb4df198c566f7097a2c9f630a9
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-07-22 00:15:33 -0400

    added comments

--- JoeAndBob2/Guy.cs
+++ JoeAndBob2/Guy.cs
@@ -5,12 +5,13 @@ class Guy
     public string Name;
     public int Cash;
 
-
+    // Writes the name and the amount of the cash the Guy has to the console.
     public void WriteMyInfo()
     {
         Console.WriteLine(Name + " has " + Cash + " bucks.");
     }
 
+    // Gives cash to other Guy, removes from wallet (prints message to console if there are not enough funds.
     public int GiveCash(int amount)
     {
         if(amount <= 0)
@@ -26,6 +27,7 @@ class Guy
         return amount;
     }
 
+    // Receives cash, adding to wallet (prints message if the amount is invalid)
     public void ReceiveCash(int amount)
     {
         if (amount <= 0)
--- JoeAndBob2/Program.cs
+++ JoeAndBob2/Program.cs
@@ -5,11 +5,14 @@ namespace JoeAndBob
 	{
 		static void Main(string[] args)
 		{
+			// new Guy Object for Joe
 			Guy joe = new Guy() { Cash = 50, Name = "Joe" };
+			// new Guy Object for Bob
 			Guy bob = new Guy() { Cash = 100, Name = "Bob" };
 
 			while (true)
 			{
+				// calls WriteMyInfo methods for each Guy Object
 				joe.WriteMyInfo();
 				bob.WriteMyInfo();
 				Console.Write("Enter an amount: ");
@@ -21,12 +24,14 @@ namespace JoeAndBob
 					string whichGuy = Console.ReadLine();
 					if (whichGuy == "Joe")
 					{
+						// calls joe object's GiveCash method, calls bob object's ReceiveCash method, saved results
 						int cashGiven = joe.GiveCash(amount);
 						bob.ReceiveCash(cashGiven);
 					}
 					else if (whichGuy == "Bob")
 					{
-						int cashGiven = bob.GiveCash(amount);
+                        // calls bob object's GiveCash method, calls joe object's ReceiveCash method, saved results
+                        int cashGiven = bob.GiveCash(amount);
 						joe.ReceiveCash(cashGiven);
 					}
 					else
